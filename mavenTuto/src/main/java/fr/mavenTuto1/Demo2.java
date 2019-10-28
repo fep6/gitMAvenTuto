09cd12f29b7d02675fee1af6eb26b5c70403a41d
fep <franck.en.planck@gmail.com>
2019-10-28 14:16:45 +0100

    mef - essai en static

---
+++ mavenTuto/src/main/java/fr/mavenTuto1/Demo2.java
@@ -0,0 +1,7 @@
+package fr.mavenTuto1;
+
+public class Demo2 {
+	 public static boolean getBool() {
+		 return false;
+	 }
+}
--- mavenTuto/src/test/java/fr/mavenTuto1/DemoTest.java
+++ mavenTuto/src/test/java/fr/mavenTuto1/DemoTest.java
@@ -5,12 +5,14 @@ import org.junit.Test;
 
 import fr.mavenTuto1.Demo;
 
-// import static src.main.java.mavenTuto1.Demo;
+// import static src.main.java.fr.mavenTuto1.*;
 
 public class DemoTest {
 	@Test
 	public void devraitRetournerVrai() {
 		Demo demo = new Demo();
+		assertTrue(Demo2.getBool());
 		assertTrue(demo.getBool());
+		System.out.println(demo.getBool());
 	}
 }
